f386c132f74c0776fb57a8ad0afac5bac65be63a
Jeff Hawkins <18685023+HumanJHawkins@users.noreply.github.com>
2026-03-12 22:57:56 -0700

    Auto-backup: Thu 12 Mar 22:57:56 PDT 2026

--- README.md
+++
@@ -1,3 +0,0 @@
-VoronConfig
-
-Generally automatically committed after changes. This repo will likely be a bit chaotic.
--- autocommit.sh
+++ autocommit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~/printer_data/config
+cd ~/printer_data
 git add .
 git commit -m "Auto-backup: $(date)"
 git push -u origin main
